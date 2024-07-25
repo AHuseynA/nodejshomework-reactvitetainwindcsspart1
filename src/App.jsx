21d42b6d08e0d5878e4b9604313cb1e56bb81b5a
AHuseynA <114856445+AHuseynA@users.noreply.github.com>
2024-07-25 16:40:09 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Nav from  "./Components/Nav"
+import Grid from "./Components/grid";
+
+
+function App() {
+    return (
+        <div>
+            <Nav></Nav>;
+            <Grid></Grid>;
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/grid.jsx
@@ -0,0 +1,26 @@
+
+function Grid() {
+    return (
+        <div className="grid grid-cols-1 m-6 gap-4 lg:grid-cols-4 lg:gap-8">
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <s className="h-24"></s>
+            <s className="h-24"></s>
+            <s className="h-24"></s>
+            <s className="h-24"></s>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+            <div className="h-32 rounded-lg bg-gray-200"></div>
+
+        </div>
+    )
+}
+
+export default Grid
